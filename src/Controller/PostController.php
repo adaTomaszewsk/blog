47b941e1c5ce9991bf9bf28a1afa2376a4abb9e1
ada <adaa.tomaszewska@gmail.com>
2023-08-30 22:47:47 +0200

    create form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,15 +6,22 @@ use App\Entity\Post;
 use App\Form\Type\PostType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_blog')]
-    public function getPosts(EntityManagerInterface $entityManager): Response
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $posts = $entityManager->getRepository(Post::class)->findAll();
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/post', name: 'post_all_posts')]
+    public function getPosts(): Response
+    {
+        $posts = $this->entityManager->getRepository(Post::class)->findAll();
             if(!$posts){
                  throw $this->createNotFoundException('No post found');
             }
@@ -26,10 +33,22 @@ class PostController extends AbstractController
     }
 
 #[Route('/post/createNew', name:'post_create_new')]
-public function createNewPost(): Response
+public function createNewPost(Request $request): Response
 {
     $post = new Post();
     $form = $this->createForm(PostType::class, $post);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $post = $form->getData();
+        $post->setCreationAt(new \DateTime('now'));
+        $this->entityManager->persist($post);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('post_all_posts');
+    }
+
+
 
     return $this->render('blog/post/new.html.twig',[
         'form' => $form,
